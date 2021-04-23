ff227568ce700ab5534bd5fc768043dfb58d2bdb
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-04-23 16:31:08 +0300

    added centralized config backbone project

---
+++ centralized-config/src/main/java/com/trackme/centralizedconfig/CentralizedConfigApplication.java
@@ -0,0 +1,13 @@
+package com.trackme.centralizedconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralizedConfigApplication.class, args);
+    }
+
+}
---
+++ centralized-config/src/test/java/com/trackme/centralizedconfig/CentralizedConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackme.centralizedconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizedConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
