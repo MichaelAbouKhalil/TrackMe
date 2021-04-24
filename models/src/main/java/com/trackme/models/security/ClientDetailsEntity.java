f4ac2ce9809daaefd4a15e028fddc71f7c8e377b
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-04-24 19:40:39 +0300

    changed client details to match oauth specs, and fixed values from preventing user to be auto-locked

--- models/src/main/java/com/trackme/models/security/ClientDetailsEntity.java
+++ models/src/main/java/com/trackme/models/security/ClientDetailsEntity.java
@@ -15,16 +15,37 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "client_details")
+@Table(name = "oauth_client_details")
 public class ClientDetailsEntity {
 
     @Id
     @Column(name = "client_id")
     private String clientId;
 
-    @Column(name = "clientSecret")
+    @Column(name = "resource_ids")
+    private String resourceIds;
+
+    @Column(name = "client_secret", length = 4000)
     private String clientSecret;
 
-    @Column(name = "authorizedGrantTypes")
+    private String scope;
+
+    @Column(name = "authorized_grant_types")
     private String authorizedGrantTypes;
+
+    @Column(name = "web_server_redirect_uri")
+    private String webServerRedirectUri;
+
+    private String authorities;
+
+    @Column(name = "access_token_validity")
+    private Integer accessTokenValidity;
+
+    @Column(name = "refresh_token_validity")
+    private Integer refreshTokenValidity;
+
+    @Column(name = "additional_information")
+    private String additionalInformation;
+
+    private String autoapprove;
 }
--- models/src/main/java/com/trackme/models/security/UserDetail.java
+++ models/src/main/java/com/trackme/models/security/UserDetail.java
@@ -44,31 +44,31 @@ public class UserDetail implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return this.password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return this.username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
