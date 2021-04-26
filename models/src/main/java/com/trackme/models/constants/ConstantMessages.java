038d574414dbcdf605705e8afc38f0354b679a6f
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-04-26 18:06:36 +0300

    added user successfullt saved to Constant Messages

--- models/src/main/java/com/trackme/models/constants/ConstantMessages.java
+++ models/src/main/java/com/trackme/models/constants/ConstantMessages.java
@@ -5,6 +5,9 @@ public class ConstantMessages {
     public static final String SUCCESS = "success";
     public static final String ERROR = "error";
     public static final String UNKNOWN_ERROR = "unknown error";
+    public static final String USER_NOT_SAVED = "user not saved. please try again later";
+
+
     public static final Integer HTTP_OK = 200;
     public static final Integer HTTP_BAD_REQUEST = 500;
 }
