19d5e547e37f942696b4a95e1dd6359b02ff498d
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-04-26 18:09:23 +0300

    added role repo

--- auth-service/src/main/java/com/trackme/authservice/repository/RoleRepository.java
+++ auth-service/src/main/java/com/trackme/authservice/repository/RoleRepository.java
@@ -4,6 +4,10 @@ import com.trackme.models.security.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+
+    Optional<RoleEntity> findByRoleName(String roleName);
 }
