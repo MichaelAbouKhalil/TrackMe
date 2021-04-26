b2f51e198ac96a3316213d2e42bff00cc4117ac9
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-04-26 18:05:57 +0300

    added SignupRequest

---
+++ models/src/main/java/com/trackme/models/payload/request/signup/SignupRequest.java
@@ -0,0 +1,40 @@
+package com.trackme.models.payload.request.signup;
+
+import com.trackme.common.constant.RegexValidationConstants;
+import com.trackme.common.validator.EmailValidator;
+import com.trackme.common.validator.ValidateEnum;
+import com.trackme.models.enums.PendingRoleEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SignupRequest {
+
+    @NotBlank(message = "Username cannot be empty")
+    @Size(min = 6, max = 40, message = "Username length must be between 6 and 40")
+    private String username;
+
+    @Email(regexp = RegexValidationConstants.EMAIL_REGEX, message = "Email is not valid")
+    private String email;
+
+    @NotBlank(message = "Password must not be blank")
+    @Pattern(regexp = RegexValidationConstants.BCRYPT_PATTERN_REGEX, message = "Password is not encrypted")
+    private String password;
+
+    @ValidateEnum(targetClassType = PendingRoleEnum.class,
+            message = "Please select a valid role")
+    private String role;
+
+    @NotBlank(message = "OrgId must not be blank")
+    private String orgId;
+}
