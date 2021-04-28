a8efe4adec640677bfb77e1375a092ea0b1404d6
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-04-28 11:52:23 +0300

    added global method security

--- auth-service/src/main/java/com/trackme/authservice/conf/security/resource/ResourceServiceConf.java
+++ auth-service/src/main/java/com/trackme/authservice/conf/security/resource/ResourceServiceConf.java
@@ -1,10 +1,12 @@
 package com.trackme.authservice.conf.security.resource;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServiceConf extends ResourceServerConfigurerAdapter {
 
     @Override
