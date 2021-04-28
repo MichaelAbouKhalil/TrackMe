711a5795589ae6f6e4f4cd5669e173c573be07cc
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-04-28 12:00:53 +0300

    active test profile and randomizing ports

--- auth-service/src/test/java/com/trackme/authservice/controller/BaseIT.java
+++ auth-service/src/test/java/com/trackme/authservice/controller/BaseIT.java
@@ -3,12 +3,14 @@ package com.trackme.authservice.controller;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
+@ActiveProfiles("test")
 public abstract class BaseIT {
 
     @Autowired
