0bfb6f8b5ad1a2cb96a63fc547693ddae4921d39
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-04-28 11:45:12 +0300

    added get for authorities

--- models/src/main/java/com/trackme/models/security/UserDetail.java
+++ models/src/main/java/com/trackme/models/security/UserDetail.java
@@ -39,7 +39,7 @@ public class UserDetail implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return this.authorities;
     }
 
     @Override
