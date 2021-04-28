d6a263e71a438a5307e24ccbf63d18f42ebf64a8
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-04-28 17:02:08 +0300

    added request validation

--- auth-service/src/main/java/com/trackme/authservice/controller/RoleUpdateController.java
+++ auth-service/src/main/java/com/trackme/authservice/controller/RoleUpdateController.java
@@ -2,6 +2,7 @@ package com.trackme.authservice.controller;
 
 import com.trackme.authservice.service.RoleService;
 import com.trackme.models.common.CommonResponse;
+import com.trackme.models.enums.RoleEnum;
 import com.trackme.models.payload.request.roleupdate.RoleUpdateRequest;
 import com.trackme.models.security.permission.PmPromotePermission;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequiredArgsConstructor
 @Slf4j
@@ -19,26 +22,26 @@ public class RoleUpdateController {
     private final RoleService roleService;
 
     @PostMapping("/role/promote")
-    public ResponseEntity<CommonResponse> promote(@RequestBody RoleUpdateRequest request){
-        CommonResponse response = roleService.promote(request);
+    public ResponseEntity<CommonResponse> promote(@Valid @RequestBody RoleUpdateRequest request){
+        CommonResponse response = roleService.promote(request, null);
         return ResponseEntity.ok().body(response);
     }
 
     @PostMapping("/role/promote/pm")
     @PmPromotePermission
-    public ResponseEntity<CommonResponse> promotePm(@RequestBody RoleUpdateRequest request){
-        CommonResponse response = roleService.promote(request);
+    public ResponseEntity<CommonResponse> promotePm(@Valid @RequestBody RoleUpdateRequest request){
+        CommonResponse response = roleService.promote(request, RoleEnum.PM);
         return ResponseEntity.ok().body(response);
     }
 
     @PostMapping("/role/demote")
-    public ResponseEntity<CommonResponse> demote(@RequestBody RoleUpdateRequest request){
+    public ResponseEntity<CommonResponse> demote(@Valid @RequestBody RoleUpdateRequest request){
 
         return null;
     }
 
     @PostMapping("/role/demote/pm")
-    public ResponseEntity<CommonResponse> demotePm(@RequestBody RoleUpdateRequest request){
+    public ResponseEntity<CommonResponse> demotePm(@Valid @RequestBody RoleUpdateRequest request){
 
         return null;
     }
