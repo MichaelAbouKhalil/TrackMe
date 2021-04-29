29f5f01a925d250052d505d8162b79479723bf88
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-04-29 20:40:06 +0300

    added comment

--- models/src/main/java/com/trackme/models/security/permission/PmPromotePermission.java
+++ models/src/main/java/com/trackme/models/security/permission/PmPromotePermission.java
@@ -6,6 +6,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * this permission is to enable ADMIN
+ * to promote PM_PENDING to PM
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @PreAuthorize("hasRole('ROLE_ADMIN')")
 public @interface PmPromotePermission {
