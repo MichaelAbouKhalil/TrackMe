ac8d0abd17e42d65d5f9ebd02c6126a890338e8c
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-04-29 22:21:15 +0300

    added constant

--- models/src/main/java/com/trackme/models/constants/ConstantMessages.java
+++ models/src/main/java/com/trackme/models/constants/ConstantMessages.java
@@ -7,6 +7,7 @@ public class ConstantMessages {
     public static final String UNKNOWN_ERROR = "unknown error";
     public static final String USER_NOT_SAVED = "user not saved. please try again later";
 
+    public static final String PENDING_ROLE_SUFFIX = "_PENDING";
 
     public static final Integer HTTP_OK = 200;
     public static final Integer HTTP_BAD_REQUEST = 500;
