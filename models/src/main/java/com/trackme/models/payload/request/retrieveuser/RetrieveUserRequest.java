b9fb1c15dbe2d28a69d6987c1a79bd19325f459f
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-01 17:48:19 +0300

    added validation

--- models/src/main/java/com/trackme/models/payload/request/retrieveuser/RetrieveUserRequest.java
+++ models/src/main/java/com/trackme/models/payload/request/retrieveuser/RetrieveUserRequest.java
@@ -2,6 +2,8 @@ package com.trackme.models.payload.request.retrieveuser;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
@@ -12,5 +14,6 @@ import javax.validation.constraints.NotNull;
 public class RetrieveUserRequest {
 
     @NotNull(message = "Username cannot be null")
+    @NotBlank(message = "Username cannot be blank")
     private String username;
 }
