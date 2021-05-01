08c866ae07c1e8e7da320390d232af0f608b494b
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-01 22:41:32 +0300

    added config annotations

--- user-service/src/main/java/com/trackme/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/trackme/userservice/UserServiceApplication.java
@@ -2,8 +2,14 @@ package com.trackme.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableResourceServer
+@EnableFeignClients
 public class UserServiceApplication {
 
     public static void main(String[] args) {
