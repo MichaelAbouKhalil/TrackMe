ee5cd832dd52a3979e429775e1452740859d3f59
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-01 12:44:39 +0300

    added find user by username

--- auth-service/src/main/java/com/trackme/authservice/service/UserService.java
+++ auth-service/src/main/java/com/trackme/authservice/service/UserService.java
@@ -1,14 +1,17 @@
 package com.trackme.authservice.service;
 
 import com.trackme.authservice.repository.UserRepository;
+import com.trackme.authservice.utils.SecurityUtils;
 import com.trackme.models.common.CommonResponse;
 import com.trackme.models.constants.ConstantMessages;
+import com.trackme.models.payload.request.retrieveuser.RetrieveUserRequest;
 import com.trackme.models.security.RoleEntity;
 import com.trackme.models.security.UserEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.common.exceptions.UnauthorizedUserException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -22,7 +25,29 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public UserEntity findUserByEmail(String email){
+    public UserEntity findUserByUsername(RetrieveUserRequest request) {
+
+        log.info("making user is retrieving their info");
+        String authUsername = SecurityUtils.getUsername();
+
+        if (!authUsername.equals(request.getUsername())) {
+            log.error("User [{}] not allowed to retrieve user [{}] info",
+                    authUsername, request.getUsername());
+            throw new UnauthorizedUserException("User is not allowed to retrieve " + request.getUsername() + "'s into");
+        }
+
+        log.info("finding user for username [{}]", request.getUsername());
+
+        UserEntity userEntity = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(
+                        () -> new UsernameNotFoundException(
+                                "user with username [" + request.getUsername() + "] is not found")
+                );
+
+        return userEntity;
+    }
+
+    public UserEntity findUserByEmail(String email) {
         log.info("finding user with email [{}]", email);
         UserEntity user = userRepository.findByEmail(email).orElseThrow(
                 () -> new UsernameNotFoundException("User with email [" + email + "] is not found.")
@@ -30,7 +55,7 @@ public class UserService {
         return user;
     }
 
-    public UserEntity updateUserRoles(UserEntity user, RoleEntity role){
+    public UserEntity updateUserRoles(UserEntity user, RoleEntity role) {
 
         user.setRoles(new ArrayList<>());
         user.getRoles().add(role);
