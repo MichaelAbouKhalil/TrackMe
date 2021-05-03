bbbbb4eb3c30bca2f2d662ece51b623399d969a4
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-03 15:35:39 +0300

    user service to get user entity from auth service

---
+++ user-service/src/main/java/com/trackme/userservice/service/UserService.java
@@ -0,0 +1,33 @@
+package com.trackme.userservice.service;
+
+import com.trackme.common.security.SecurityUtils;
+import com.trackme.models.common.CommonResponse;
+import com.trackme.models.security.UserEntity;
+import com.trackme.userservice.proxy.AuthServiceFeignProxy;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserService {
+
+    private final AuthServiceFeignProxy authServiceFeignProxy;
+
+    public UserEntity getUser() {
+        UserEntity user = null;
+
+        log.info("retrieving user info from auth service for username [{}]", SecurityUtils.getUsername());
+        CommonResponse response = authServiceFeignProxy.retrieveUser();
+
+        if (response.isSuccess()) {
+            log.info("user [{}] found with id [{}]", user.getUsername(), user.getId());
+            user = (UserEntity) response.getPayload();
+        } else {
+            log.error("user [{}] not found", SecurityUtils.getUsername());
+        }
+
+        return user;
+    }
+}
