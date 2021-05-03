5dddb815e903e666d212a5807c0d7e338eb2a96c
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-03 15:34:36 +0300

    added entity scan

--- user-service/src/main/java/com/trackme/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/trackme/userservice/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.trackme.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -10,6 +11,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableDiscoveryClient
 @EnableResourceServer
 @EnableFeignClients
+@EntityScan(basePackages = {"com.trackme.models.profile"})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
