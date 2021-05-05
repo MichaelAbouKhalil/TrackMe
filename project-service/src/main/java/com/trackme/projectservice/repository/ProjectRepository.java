27dc0a44de21483608e7369568824f9606f7630a
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-05 22:26:42 +0300

    added project repository

--- project-service/src/main/java/com/trackme/projectservice/repository/ProjectRepository.java
+++ project-service/src/main/java/com/trackme/projectservice/repository/ProjectRepository.java
@@ -4,6 +4,8 @@ import com.trackme.models.project.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
 }
