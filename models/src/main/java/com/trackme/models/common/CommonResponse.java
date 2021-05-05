ce61b9b94ce8cc5fc5812d20f54c427ab1efa787
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-05 16:21:00 +0300

    added generic type to CommonResponse

--- models/src/main/java/com/trackme/models/common/CommonResponse.java
+++ models/src/main/java/com/trackme/models/common/CommonResponse.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.trackme.models.constants.ConstantMessages;
 import lombok.*;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,32 +20,32 @@ import lombok.*;
         "payload",
         "error"
 })
-public class CommonResponse {
+public class CommonResponse<T extends Object> implements Serializable {
 
     private int status;
     private boolean success;
     private String message;
-    private Object payload;
+    private T payload;
     private ErrorResponse error;
 
-    public static CommonResponse ok() {
+    public static <T> CommonResponse<T> ok() {
         return ok(ConstantMessages.HTTP_OK, ConstantMessages.SUCCESS, null);
     }
 
-    public static CommonResponse ok(Object payload) {
+    public static <T> CommonResponse<T> ok(T payload) {
         return ok(ConstantMessages.HTTP_OK, ConstantMessages.SUCCESS, payload);
     }
 
-    public static CommonResponse ok(String message, Object payload) {
+    public static <T> CommonResponse<T> ok(String message, T payload) {
         return ok(ConstantMessages.HTTP_OK, message, payload);
     }
 
-    public static CommonResponse ok(int status, Object payload) {
+    public static <T> CommonResponse<T> ok(int status, T payload) {
         return ok(status, ConstantMessages.SUCCESS, payload);
     }
 
-    public static CommonResponse ok(int status, String message, Object payload) {
-        return CommonResponse.builder()
+    public static <T> CommonResponse<T> ok(int status, String message, T payload) {
+        return CommonResponse.<T>builder()
                 .success(Boolean.TRUE)
                 .status(status)
                 .message(message)
@@ -51,28 +53,28 @@ public class CommonResponse {
                 .build();
     }
 
-    public static CommonResponse error() {
+    public static <T> CommonResponse<T> error() {
         return error(ConstantMessages.HTTP_BAD_REQUEST, ConstantMessages.UNKNOWN_ERROR, null);
     }
 
-    public static CommonResponse error(Object payload) {
+    public static <T> CommonResponse<T> error(T payload) {
         return error(ConstantMessages.HTTP_BAD_REQUEST, ConstantMessages.UNKNOWN_ERROR, payload);
     }
 
-    public static CommonResponse error(String errorMessage, Object payload) {
+    public static <T> CommonResponse<T> error(String errorMessage, T payload) {
         return error(ConstantMessages.HTTP_BAD_REQUEST, errorMessage, payload);
     }
 
-    public static CommonResponse error(int status, Object payload) {
+    public static <T> CommonResponse<T> error(int status, T payload) {
         return error(status, ConstantMessages.UNKNOWN_ERROR, payload);
     }
 
-    public static CommonResponse error(int status, String errorMessage) {
+    public static <T> CommonResponse<T> error(int status, String errorMessage) {
         return error(status, errorMessage, null);
     }
 
-    public static CommonResponse error(int status, String errorMessage, Object payload) {
-        return CommonResponse.builder()
+    public static <T> CommonResponse<T> error(int status, String errorMessage, T payload) {
+        return CommonResponse.<T>builder()
                 .success(Boolean.FALSE)
                 .status(status)
                 .message(ConstantMessages.ERROR)
