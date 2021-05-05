919f059b999bed1b34760b7060a79a0115a29548
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-05 22:26:13 +0300

    added ProjectDbService to handle actions with database and errors that may occur

---
+++ project-service/src/main/java/com/trackme/projectservice/service/ProjectDbService.java
@@ -0,0 +1,58 @@
+package com.trackme.projectservice.service;
+
+import com.trackme.models.exception.InvalidOperationException;
+import com.trackme.models.exception.ProjectNotFoundException;
+import com.trackme.models.project.ProjectEntity;
+import com.trackme.projectservice.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ProjectDbService {
+
+    private final ProjectRepository projectRepository;
+
+    /**
+     * retrieve project from database
+     * @param projectId
+     * @return
+     */
+    public ProjectEntity findProject(Long projectId){
+        log.info("locating project with id [{}]", projectId);
+        ProjectEntity project = projectRepository.findById(projectId)
+                .orElseThrow(() ->
+                        new ProjectNotFoundException("project with id [" + projectId + "] is not found."));
+
+        return project;
+    }
+
+    /**
+     * save project in database
+     * @param project
+     * @return
+     */
+    public ProjectEntity saveProject(ProjectEntity project){
+        log.info("saving project with name [{}]", project.getName());
+        ProjectEntity savedProject = projectRepository.save(project);
+
+        if (savedProject == null) {
+            throw new RuntimeException("Failed to save project in database");
+        }
+        return savedProject;
+    }
+
+    /**
+     * delete project in database
+     * @param project
+     * @return
+     */
+    public void deleteProject(ProjectEntity project){
+
+        log.info("deleting project with id [{}] and name [{}]", project.getId(), project.getName());
+        projectRepository.deleteById(project.getId());
+    }
+
+}
