1db965871eccabe4a68bdf310ba1d10322709fbb
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-08 19:05:21 +0300

    added api to find user details

--- auth-service/src/main/java/com/trackme/authservice/controller/UserController.java
+++ auth-service/src/main/java/com/trackme/authservice/controller/UserController.java
@@ -2,13 +2,17 @@ package com.trackme.authservice.controller;
 
 import com.trackme.authservice.service.AuthUserService;
 import com.trackme.models.common.CommonResponse;
+import com.trackme.models.payload.request.retrieveuser.GetUserDetailsRequest;
 import com.trackme.models.security.UserEntity;
 import com.trackme.models.security.permission.AuthenticatedPermission;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,9 +25,20 @@ public class UserController {
     @AuthenticatedPermission
     public ResponseEntity<CommonResponse> retrieveUser() {
         log.info("received request on retrieveUser()");
-        UserEntity user = authUserService.findUserByUsername();
+        UserEntity user = authUserService.findAuthUser();
 
         CommonResponse response = CommonResponse.ok(user);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/userDetails")
+    @AuthenticatedPermission
+    public ResponseEntity<CommonResponse> getUserDetails(@Valid @RequestBody GetUserDetailsRequest request) {
+        log.info("received request on getUserDetails()");
+        UserEntity user = authUserService.findByUsernameOrEmail(request);
+
+        CommonResponse<UserEntity> response = CommonResponse.ok(user);
 
         return ResponseEntity.ok().body(response);
     }
--- auth-service/src/test/java/com/trackme/authservice/controller/UserControllerTest.java
+++ auth-service/src/test/java/com/trackme/authservice/controller/UserControllerTest.java
@@ -1,13 +1,10 @@
 package com.trackme.authservice.controller;
 
 import com.trackme.authservice.service.AuthUserService;
-import com.trackme.models.payload.request.retrieveuser.RetrieveUserRequest;
+import com.trackme.models.payload.request.retrieveuser.GetUserDetailsRequest;
 import com.trackme.models.security.UserEntity;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,19 +18,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Slf4j
 class UserControllerTest extends BaseController {
 
-    private static final String BASE_API = "/user";
+    private static final String BASE_API_AUTH_USER = "/user";
+    private static final String BASE_API_USER_DETAILS = "/userDetails";
 
     @MockBean
     AuthUserService authUserService;
 
     UserEntity user;
-    RetrieveUserRequest request;
+    GetUserDetailsRequest getUserDetailsRequest;
 
     @BeforeEach
     void setUp() {
         user = UserEntity.builder()
                 .username("test-username").build();
-        request = RetrieveUserRequest.builder().username("test").build();
+        getUserDetailsRequest = GetUserDetailsRequest.builder()
+                .email("test@email.com").build();
     }
 
     @DisplayName("Retrieve User Access Tests")
@@ -41,11 +40,10 @@ class UserControllerTest extends BaseController {
     class RetrieveUserAccessTests {
         @Test
         public void retrieveUser_NoAuth_Invalid() throws Exception {
-            when(authUserService.findUserByUsername())
+            when(authUserService.findAuthUser())
                     .thenReturn(user);
 
-            mockMvc.perform(get(BASE_API)
-                    .content(objectMapper.writeValueAsString(request))
+            mockMvc.perform(get(BASE_API_AUTH_USER)
             )
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.success").value("false"))
@@ -56,16 +54,11 @@ class UserControllerTest extends BaseController {
 
         @Test
         public void retrieveUser_ADMIN_Valid() throws Exception {
-            when(authUserService.findUserByUsername())
+            when(authUserService.findAuthUser())
                     .thenReturn(user);
 
-            String accessToken = accessTokenUtil.obtainAccessToken(
-                    "demo-admin", "demo-admin");
-
-            ResultActions result = mockMvc.perform(get(BASE_API)
-                    .content(objectMapper.writeValueAsString(request))
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .header("AUTHORIZATION", "Bearer " + accessToken)
+            ResultActions result = mockMvc.perform(get(BASE_API_AUTH_USER)
+                    .header("AUTHORIZATION", "Bearer " + adminToken)
             )
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.success").value("true"))
@@ -77,16 +70,11 @@ class UserControllerTest extends BaseController {
 
         @Test
         public void retrieveUser_PM_Valid() throws Exception {
-            when(authUserService.findUserByUsername())
+            when(authUserService.findAuthUser())
                     .thenReturn(user);
 
-            String accessToken = accessTokenUtil.obtainAccessToken(
-                    "demo-pm", "demo-pm");
-
-            ResultActions result = mockMvc.perform(get(BASE_API)
-                    .content(objectMapper.writeValueAsString(request))
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .header("AUTHORIZATION", "Bearer " + accessToken)
+            ResultActions result = mockMvc.perform(get(BASE_API_AUTH_USER)
+                    .header("AUTHORIZATION", "Bearer " + pmToken)
             )
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.success").value("true"))
@@ -98,16 +86,11 @@ class UserControllerTest extends BaseController {
 
         @Test
         public void retrieveUser_DEV_Valid() throws Exception {
-            when(authUserService.findUserByUsername())
+            when(authUserService.findAuthUser())
                     .thenReturn(user);
 
-            String accessToken = accessTokenUtil.obtainAccessToken(
-                    "demo-dev", "demo-dev");
-
-            ResultActions result = mockMvc.perform(get(BASE_API)
-                    .content(objectMapper.writeValueAsString(request))
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .header("AUTHORIZATION", "Bearer " + accessToken)
+            ResultActions result = mockMvc.perform(get(BASE_API_AUTH_USER)
+                    .header("AUTHORIZATION", "Bearer " + devToken)
             )
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.success").value("true"))
@@ -119,16 +102,11 @@ class UserControllerTest extends BaseController {
 
         @Test
         public void retrieveUser_CUSTOMER_Valid() throws Exception {
-            when(authUserService.findUserByUsername())
+            when(authUserService.findAuthUser())
                     .thenReturn(user);
 
-            String accessToken = accessTokenUtil.obtainAccessToken(
-                    "demo-customer", "demo-customer");
-
-            ResultActions result = mockMvc.perform(get(BASE_API)
-                    .content(objectMapper.writeValueAsString(request))
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .header("AUTHORIZATION", "Bearer " + accessToken)
+            ResultActions result = mockMvc.perform(get(BASE_API_AUTH_USER)
+                    .header("AUTHORIZATION", "Bearer " + custToken)
             )
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.success").value("true"))
@@ -136,6 +114,90 @@ class UserControllerTest extends BaseController {
                     .andExpect(jsonPath("$.payload").exists())
                     .andExpect(jsonPath("$.error").doesNotExist());
 
+        }
+    }
+
+    @DisplayName("Get User Details Access Tests")
+    @Nested
+    class GetUserDetailsAccessTests {
+        @Test
+        public void getUserDetails_NoAuth_Invalid() throws Exception {
+            when(authUserService.findByUsernameOrEmail(any(GetUserDetailsRequest.class)))
+                    .thenReturn(user);
+
+            mockMvc.perform(get(BASE_API_USER_DETAILS)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(getUserDetailsRequest)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status").value(HttpStatus.UNAUTHORIZED.value()))
+                    .andExpect(jsonPath("$.success").value(Boolean.FALSE))
+                    .andExpect(jsonPath("$.error").exists())
+                    .andExpect(jsonPath("$.error.errorMessage").exists())
+                    .andExpect(jsonPath("$.payload").doesNotExist());
+        }
+
+        @Test
+        public void getUserDetails_ADMIN_Valid() throws Exception {
+            when(authUserService.findByUsernameOrEmail(any(GetUserDetailsRequest.class)))
+                    .thenReturn(user);
+
+            mockMvc.perform(get(BASE_API_USER_DETAILS)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("AUTHORIZATION", "Bearer " + adminToken)
+                    .content(objectMapper.writeValueAsString(getUserDetailsRequest)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
+                    .andExpect(jsonPath("$.success").value(Boolean.TRUE))
+                    .andExpect(jsonPath("$.payload").exists())
+                    .andExpect(jsonPath("$.error").doesNotExist());
+        }
+
+        @Test
+        public void getUserDetails_PM_Valid() throws Exception {
+            when(authUserService.findByUsernameOrEmail(any(GetUserDetailsRequest.class)))
+                    .thenReturn(user);
+
+            mockMvc.perform(get(BASE_API_USER_DETAILS)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("AUTHORIZATION", "Bearer " + pmToken)
+                    .content(objectMapper.writeValueAsString(getUserDetailsRequest)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
+                    .andExpect(jsonPath("$.success").value(Boolean.TRUE))
+                    .andExpect(jsonPath("$.payload").exists())
+                    .andExpect(jsonPath("$.error").doesNotExist());
+        }
+
+        @Test
+        public void getUserDetails_DEV_Valid() throws Exception {
+            when(authUserService.findByUsernameOrEmail(any(GetUserDetailsRequest.class)))
+                    .thenReturn(user);
+
+            mockMvc.perform(get(BASE_API_USER_DETAILS)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("AUTHORIZATION", "Bearer " + devToken)
+                    .content(objectMapper.writeValueAsString(getUserDetailsRequest)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
+                    .andExpect(jsonPath("$.success").value(Boolean.TRUE))
+                    .andExpect(jsonPath("$.payload").exists())
+                    .andExpect(jsonPath("$.error").doesNotExist());
+        }
+
+        @Test
+        public void getUserDetails_CUST_Valid() throws Exception {
+            when(authUserService.findByUsernameOrEmail(any(GetUserDetailsRequest.class)))
+                    .thenReturn(user);
+
+            mockMvc.perform(get(BASE_API_USER_DETAILS)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("AUTHORIZATION", "Bearer " + custToken)
+                    .content(objectMapper.writeValueAsString(getUserDetailsRequest)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
+                    .andExpect(jsonPath("$.success").value(Boolean.TRUE))
+                    .andExpect(jsonPath("$.payload").exists())
+                    .andExpect(jsonPath("$.error").doesNotExist());
         }
     }
 }
