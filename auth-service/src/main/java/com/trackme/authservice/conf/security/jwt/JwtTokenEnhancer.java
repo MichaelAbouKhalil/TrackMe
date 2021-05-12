8b9ce5b5e6c790cdafd5a9bea602096bc2c44ed0
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-12 15:51:30 +0300

    added info to jwt token to be used later

--- auth-service/src/main/java/com/trackme/authservice/conf/security/jwt/JwtTokenEnhancer.java
+++ auth-service/src/main/java/com/trackme/authservice/conf/security/jwt/JwtTokenEnhancer.java
@@ -1,5 +1,6 @@
 package com.trackme.authservice.conf.security.jwt;
 
+import com.trackme.models.security.UserDetail;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
@@ -17,6 +18,13 @@ public class JwtTokenEnhancer implements TokenEnhancer {
         /**
          * If we want to add additional info to the token
          */
+        if(oAuth2Authentication.getPrincipal() != null &&
+                oAuth2Authentication.getPrincipal() instanceof UserDetail){
+            // Not used at the moment vvv
+            UserDetail userDetail = (UserDetail) oAuth2Authentication.getPrincipal();
+            additionalInfo.put("userId", userDetail.getUserId());
+            additionalInfo.put("userEmail", userDetail.getEmail());
+        }
 
         ((DefaultOAuth2AccessToken) oAuth2AccessToken).setAdditionalInformation(additionalInfo);
         return oAuth2AccessToken;
