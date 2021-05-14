b628e8583e16a6fc7dada7df1da12da556b70b34
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-14 20:29:10 +0300

    get ticket implementation

--- ticket-service/src/main/java/com/trackme/ticketservice/service/TicketService.java
+++ ticket-service/src/main/java/com/trackme/ticketservice/service/TicketService.java
@@ -1,10 +1,15 @@
 package com.trackme.ticketservice.service;
 
+import com.trackme.common.service.ProjectProxyService;
+import com.trackme.common.service.UserService;
 import com.trackme.models.common.CommonResponse;
 import com.trackme.models.payload.request.ticket.AssignRemoveTicketRequest;
 import com.trackme.models.payload.request.ticket.CreateTicketRequest;
 import com.trackme.models.payload.request.ticket.UpdateTicketRequest;
+import com.trackme.models.project.ProjectEntity;
+import com.trackme.models.security.UserEntity;
 import com.trackme.models.ticket.TicketEntity;
+import com.trackme.ticketservice.utils.TicketUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,9 +20,21 @@ import org.springframework.stereotype.Service;
 public class TicketService {
 
     private final TicketDbService ticketDbService;
+    private final UserService userService;
+    private final ProjectProxyService projectProxyService;
 
     public CommonResponse<TicketEntity> getTicket(Long id) {
-        return null;
+
+        UserEntity user = userService.getUser();
+
+        TicketEntity ticket = ticketDbService.getTicket(id);
+
+        // check if user assigned to project for this ticket
+        ProjectEntity project = projectProxyService.getProject(ticket.getProjectId());
+
+        TicketUtils.checkIfUserAssignToProject(user, project);
+
+        return CommonResponse.ok(ticket);
     }
 
     public CommonResponse<TicketEntity> createTicket(CreateTicketRequest request) {
