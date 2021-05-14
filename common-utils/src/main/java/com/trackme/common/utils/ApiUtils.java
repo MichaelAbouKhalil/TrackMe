48e8501ef183242049feb90e90922404f8c0d28a
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-14 20:28:40 +0300

    method to get project entity from response entity

--- common-utils/src/main/java/com/trackme/common/utils/ApiUtils.java
+++ common-utils/src/main/java/com/trackme/common/utils/ApiUtils.java
@@ -3,6 +3,7 @@ package com.trackme.common.utils;
 import com.trackme.common.security.SecurityUtils;
 import com.trackme.common.service.UserService;
 import com.trackme.models.common.CommonResponse;
+import com.trackme.models.project.ProjectEntity;
 import com.trackme.models.security.UserEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -32,5 +33,27 @@ public class ApiUtils {
         }
 
         return user;
+    }
+
+    public static ProjectEntity getProjectFromResponseEntity(ResponseEntity<CommonResponse<ProjectEntity>> response){
+        ProjectEntity project;
+
+        if (!response.getStatusCode().equals(HttpStatus.OK)) {
+            log.error("request failed to auth service, status [{}]", response.getStatusCode());
+            throw new RuntimeException("Error with retrieving project from Project service");
+        }
+
+        if (response.getBody().isSuccess()) {
+            CommonResponse body = response.getBody();
+            project = (ProjectEntity) body.getPayload();
+            log.info("project [{}] found with id [{}]", project.getName(), project.getId());
+        } else {
+            log.error("project not found");
+            log.error("project not found with response status [{}] and response error message [{}]",
+                    response.getBody().getStatus(), response.getBody().getError().getErrorMessage());
+            throw new UsernameNotFoundException(response.getBody().getError().getErrorMessage());
+        }
+
+        return project;
     }
 }
