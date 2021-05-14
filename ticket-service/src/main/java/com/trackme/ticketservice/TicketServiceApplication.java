f04fe2f94bdf7404f5b358e0ea8bdaf346ad91f1
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-14 16:42:26 +0300

    configuration and properties for ticket service

--- ticket-service/src/main/java/com/trackme/ticketservice/TicketServiceApplication.java
+++ ticket-service/src/main/java/com/trackme/ticketservice/TicketServiceApplication.java
@@ -2,8 +2,18 @@ package com.trackme.ticketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableResourceServer
+@EnableFeignClients(basePackages = {"com.trackme"})
+@EntityScan(basePackages = {"com.trackme.models.project"})
+@ComponentScan(basePackages = {"com.trackme.ticketservice", "com.trackme.common"})
 public class TicketServiceApplication {
 
     public static void main(String[] args) {
