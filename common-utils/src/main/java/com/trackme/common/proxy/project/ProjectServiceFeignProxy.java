fe5637bd191ab7ceacf8e79c63cfa40326754d8b
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-14 20:28:22 +0300

    added project feign client and service to handle requests

---
+++ common-utils/src/main/java/com/trackme/common/proxy/project/ProjectServiceFeignProxy.java
@@ -0,0 +1,23 @@
+package com.trackme.common.proxy.project;
+
+import com.trackme.common.interceptor.HeaderRequestInterceptor;
+import com.trackme.models.common.CommonResponse;
+import com.trackme.models.payload.request.project.GetProjectRequest;
+import com.trackme.models.payload.request.retrieveuser.GetUserDetailsRequest;
+import com.trackme.models.project.ProjectEntity;
+import com.trackme.models.security.UserEntity;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(
+        name = "project-service",
+        configuration = {HeaderRequestInterceptor.class}
+)
+public interface ProjectServiceFeignProxy {
+
+    @GetMapping("/project")
+    public ResponseEntity<CommonResponse<ProjectEntity>> getProject(GetProjectRequest request);
+}
+
---
+++ common-utils/src/main/java/com/trackme/common/service/ProjectProxyService.java
@@ -0,0 +1,31 @@
+package com.trackme.common.service;
+
+import com.trackme.common.proxy.project.ProjectServiceFeignProxy;
+import com.trackme.common.utils.ApiUtils;
+import com.trackme.models.common.CommonResponse;
+import com.trackme.models.payload.request.project.GetProjectRequest;
+import com.trackme.models.project.ProjectEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProjectProxyService {
+
+    private final ProjectServiceFeignProxy projectServiceFeignProxy;
+
+    public ProjectEntity getProject(Long projectId){
+        log.info("finding project info for project id [{}]", projectId);
+
+        ResponseEntity<CommonResponse<ProjectEntity>> response =
+                projectServiceFeignProxy.getProject(GetProjectRequest.builder()
+                        .projectId(projectId).build());
+
+        ProjectEntity project = ApiUtils.getProjectFromResponseEntity(response);
+
+        return project;
+    }
+}
