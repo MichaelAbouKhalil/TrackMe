e62b7d0a22a09b9ff552ff1fb7cd06762fcfe383
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-15 11:43:47 +0300

    added orgId to UserDetails, removed RoleNotFoundException

---
+++ models/src/main/java/com/trackme/models/constants/JwtFields.java
@@ -0,0 +1,8 @@
+package com.trackme.models.constants;
+
+public class JwtFields {
+
+    public static final String JWT_EMAIL = "email";
+    public static final String JWT_USER_ID = "userId";
+    public static final String JWT_ORG_ID = "orgId";
+}
