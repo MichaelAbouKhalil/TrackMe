82433a2389066417a1888c7e47abc50c4c0da9ea
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-17 18:28:58 +0300

    added hash and equal functions for AssignedEntity

--- models/src/main/java/com/trackme/models/ticket/AssignedEntity.java
+++ models/src/main/java/com/trackme/models/ticket/AssignedEntity.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -40,4 +41,17 @@ public class AssignedEntity {
     @JoinColumn(name = "closed_ticket_id")
     private TicketEntity closed;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AssignedEntity that = (AssignedEntity) o;
+        return Objects.equals(email, that.email)
+                && Objects.equals(role, that.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, role);
+    }
 }
