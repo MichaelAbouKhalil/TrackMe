b25fd25b25fc3a54bbaba824ebbc446014b89077
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2021-05-19 17:20:13 +0300

    Removed getting username from security context

--- common-utils/src/main/java/com/trackme/common/security/SecurityUtils.java
+++ common-utils/src/main/java/com/trackme/common/security/SecurityUtils.java
@@ -30,26 +30,4 @@ public class SecurityUtils {
     public static String[] getUserRoles() {
         return getJwtToken().getAuthorities();
     }
-
-    private static Object getAuthenticatedUser() {
-        if (SecurityContextHolder.getContext() == null
-                || SecurityContextHolder.getContext().getAuthentication() == null
-                || SecurityContextHolder.getContext().getAuthentication().getPrincipal() == null) {
-
-            throw new UnauthorizedUserException("Unauthorized/Unauthenticated user exception");
-        }
-
-        return SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    }
-
-    public static String getUsernameFromContext() {
-        Object authObject = getAuthenticatedUser();
-        if (authObject instanceof User) {
-            return ((User) authObject).getUsername();
-        } else if (authObject instanceof String) {
-            return (String) authObject;
-        }
-
-        throw new UnauthorizedUserException("Authentication is not instance on User or String, " + authObject);
-    }
 }
